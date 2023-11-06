cef2da8cbd494e3d69c5b7371a888d7090f591eb
gekicchi <oyarzunm18@gmail.com>
2023-11-06 17:32:11 -0300

    lab completo

--- laboratorio9.cpp
+++ laboratorio9.cpp
@@ -5,16 +5,20 @@ using namespace std;
 
 struct Node{
 	string letra;
+	int data;
 	Node *left;
 	Node *right;
 	
-	Node(string l) : letra(l), left(nullptr), right(nullptr) {}
+	Node(string l) : letra(l), data(0), left(nullptr), right(nullptr) {}
+	Node(int d) : letra(""), data(d), left(nullptr), right(nullptr) {}
 };
 
 // funciones de impresion
 void print_pre_order(Node *current);
-void print_in_order(Node *current);
+void print_in_order(Node *current, int mode);
 void print_post_order(Node *current);
+
+Node* insert_node(Node *current, int data);
 
 // funciones de busqueda
 Node* DFS(Node *current, string value);
@@ -22,28 +26,41 @@ Node* BFS(Node *root, string value);
 
 int main()
 {
-	Node *root = new Node("M");
-	root->left = new Node("C");
-	root->right = new Node("Z");
-	root->left->left = new Node("X");
-	root->left->right = new Node("O");
-	root->right->left = new Node("T");
-	root->right->right = new Node("F");
-	root->right->left->left = new Node("W");
+	// ejercicio 1
+	Node *root_letra = new Node("M");
+	root_letra->left = new Node("C");
+	root_letra->right = new Node("Z");
+	root_letra->left->left = new Node("X");
+	root_letra->left->right = new Node("O");
+	root_letra->right->left = new Node("T");
+	root_letra->right->right = new Node("F");
+	root_letra->right->left->left = new Node("W");
 	
+	// ejercicio 2
 	cout << "pre-order: ";
-	print_pre_order(root);
+	print_pre_order(root_letra);
 	cout << endl << "in-order ";
-	print_in_order(root);
+	print_in_order(root_letra, 0);
 	cout << endl << "post-order: ";
-	print_post_order(root);
+	print_post_order(root_letra);
 	cout << endl;
 	
-	Node *searchDFS = DFS(root, "O");
+	//ejercicio 3
+	int arr[10] = {21, 18, 6, 9, 10, 7, 19, 15, 12, 5};
+	Node *root_data = nullptr;
+	for (int i=0; i<10; i++)
+		root_data = insert_node(root_data, arr[i]);
+	
+	print_in_order(root_data,1);
+	cout << endl;
+	
+	// ejercicio 4
+	Node *searchDFS = DFS(root_letra, "O");
 	if (searchDFS != nullptr)
 		cout << "Encontrado: " << searchDFS->letra << endl;
 		
-	Node *searchBFS = BFS(root, "C");
+	// ejercicio 5
+	Node *searchBFS = BFS(root_letra, "C");
 	if (searchBFS != nullptr)
 		cout << "Encontrado: " << searchBFS->letra << endl;
 	
@@ -60,14 +77,17 @@ void print_pre_order(Node *current)
 	print_pre_order(current->right);
 }
 
-void print_in_order(Node *current)
+void print_in_order(Node *current, int mode)
 {
 	if (current == nullptr)
 		return;
 		
-	print_in_order(current->left);
-	cout << current->letra << "  ";
-	print_in_order(current->right);
+	print_in_order(current->left, mode);
+	if (mode == 0) // imprime string
+		cout << current->letra << "  ";
+	else // imprime int
+		cout << current->data << "  ";
+	print_in_order(current->right, mode);
 }
 void print_post_order(Node *current)
 {
@@ -77,6 +97,19 @@ void print_post_order(Node *current)
 	print_post_order(current->left);
 	print_post_order(current->right);
 	cout << current->letra << "  ";
+}
+
+Node* insert_node(Node *current, int data)
+{
+	if (current == nullptr)
+		return new Node(data);
+		
+	if (current->data > data)
+		current->left = insert_node(current->left, data);
+	else
+		current->right = insert_node(current->right, data);
+		
+	return current;
 }
 
 Node* DFS(Node *current, string value)
